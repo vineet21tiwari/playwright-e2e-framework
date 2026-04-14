749ea0bc9d1737930334a622010a7054a8f4f040
vineet21tiwari <vineet211192@gmail.com>
2026-04-14 21:47:38 +0530

    added logintest cases

---
+++ pages/LoginPage.js
@@ -0,0 +1,33 @@
+const {expect} =  require('@playwright/test');
+
+class LoginPage {
+    constructor(page){
+        this.page = page;
+        this.usernameInput = page.locator('#user-name');
+        this.passwordInput = page.locator('#password');
+        this.loginInput = page.locator('#login-button');
+        this.errorMessage = page.locator('[data-test="error"]');
+    }
+    async goto(){
+        await this.page.goto('https://www.saucedemo.com/');
+    }
+    async enterUsername(username){
+        await this.usernameInput.fill(username);
+    }
+
+     async enterPassword(password){
+        await this.passwordInput.fill(password);
+    }
+
+    async clickLogin(){
+        await this.loginInput.click();
+    }
+
+    async login(username,password){
+        await this.enterUsername(username);
+        await this.enterPassword(password);
+        await this.clickLogin();
+    }
+}
+
+module.exports = {LoginPage};
---
+++ tests/login.spec.js
@@ -0,0 +1,19 @@
+const { test, expect } = require("@playwright/test");
+const { LoginPage } = require("../pages/LoginPage");
+
+test.describe('Login Tests', ()=>{
+    test('valid login', async ({page})=>{
+        const loginPage = new LoginPage(page);
+        await loginPage.goto();
+        await loginPage.login('standard_user','secret_sauce');
+        await expect(page).toHaveURL(/inventory/);
+    });
+
+    test('invalid login', async ({page})=>{
+        const loginPage = new LoginPage(page);
+        await loginPage.goto();
+        await loginPage.login('standard_user','wrong_password');
+
+        await expect(loginPage.errorMessage).toBeVisible();
+    });
+});
